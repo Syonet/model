3c6f502a0f60b177b7b30286b6e657657dac4002
Gustavo Henke <gustavo@injoin.io>
2015-01-12 11:19:54 -0200

    test($modelConfig): create initial tests

---
+++ test/specs/config.spec.js
@@ -0,0 +1,33 @@
+describe( "$modelConfig", function () {
+    "use strict";
+
+    var cfg;
+    beforeEach( module( "syonet.model" ) );
+    beforeEach( inject(function ( $injector ) {
+        cfg = $injector.get( "$modelConfig" );
+    }));
+
+    afterEach(function () {
+        localStorage.clear();
+    });
+
+    describe( ".get()", function () {
+        it( "should return current configuration", function () {
+            localStorage.setItem( "$model.__config", JSON.stringify( "foo" ) );
+            expect( cfg.get() ).to.equal( "foo" );
+        });
+
+        it( "should return empty object if no config available", function () {
+            expect( cfg.get() ).to.eql( {} );
+        });
+    });
+
+    // ---------------------------------------------------------------------------------------------
+
+    describe( ".set( val )", function () {
+        it( "should persist value on localStorage stringified", function () {
+            cfg.set({});
+            expect( localStorage.getItem( "$model.__config" ) ).to.equal( "{}" );
+        });
+    });
+});
