c013316f6f2f93a2b868d07fed37897b81725fe1
Gustavo Henke <gustavo@injoin.io>
2015-02-09 17:22:50 -0200

    feat($modelRequest): replace temp IDs before sending requests

--- src/temp.js
+++ src/temp.js
@@ -5,20 +5,58 @@
 
     function tempService ( $modelConfig, $modelDB ) {
         var db = $modelDB( "__temp" );
+        var api = {
+            regex: /\$\$temp\d+/g
+        };
 
         /**
          * Generate the next temporary ID
          *
          * @returns {Number}
          */
-        db.next = function () {
+        api.next = function () {
             var cfg = $modelConfig.get();
             cfg.tempID = ( cfg.tempID || 0 ) + 1;
             $modelConfig.set( cfg );
 
-            return cfg.tempID;
+            return "$$temp" + cfg.tempID;
         };
 
-        return db;
+        /**
+         * Define an real ID to an temporary one
+         *
+         * @param   {String} tempID
+         * @param   {*} currID
+         * @returns {Promise}
+         */
+        api.set = function ( tempID, currID ) {
+            return db.put({
+                id: currID
+            }, tempID );
+        };
+
+        /**
+         * Return an persisted ID for an temporary one
+         *
+         * @param   {String} tempID
+         * @returns {Promise}
+         */
+        api.get = function ( tempID ) {
+            return db.get( tempID ).then(function ( doc ) {
+                return doc.id;
+            });
+        };
+
+        /**
+         * Determine if a string is a temporary ID.
+         *
+         * @param   {String} id
+         * @returns {Boolean}
+         */
+        api.is = function ( id ) {
+            return api.regex.test( id );
+        };
+
+        return api;
     }
 }();
